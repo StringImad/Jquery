67e54e34924b57eec736322cec3c238ec4ab3cab
imad <imflow99@gmail.com>
2022-11-21 09:40:12 +0100

    Subida desde clase, ejercicio login terminado

---
+++ loginJquery.php
@@ -0,0 +1,13 @@
+<?php
+
+$usuario = "usa1";
+$contra = "con";
+if($usuario==$_POST['usuario'])
+    echo "<p>Usuario correcto</p>";
+else 
+    echo "<p>Usuario incorrecto</p>";
+
+    if($contra==$_POST['contra'])
+    echo "<p>Contraseña correcta</p>";
+else 
+    echo "<p>Contraseña incorrecta</p>";
